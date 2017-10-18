64798ccf1b4be74a3c3e30b824845f26d718b2dc
abel <abel@abeldeMacBook-Pro.local>
2017-10-18 19:11:56 +0800

    add

--- gulpfile.js
+++ gulpfile.js
@@ -4,38 +4,77 @@ var argv = require('yargs').argv;
 var $ = gulpLoadPlugins();
 
 var browserSync = require('browser-sync');
-var reload      = browserSync.reload;
+var reload = browserSync.reload;
+
+var currentPath = 'together_always/'
 
 /**
  * argv
  *  -p --path 改变serve的根目录 默认 .
  *  
  */
-var baseDir = argv.path || argv.P || '.';
+var baseDir = argv.path || argv.P || currentPath;
+
+baseDir = 'src/' + baseDir
 
 gulp.task('serve:dev', function() {
-    browserSync.init({
-        notify: false,
-        port: 9001,
-        open: false,
-        server: {
-            baseDir: [ baseDir ],
-        }
-    })
-    gulp.watch([baseDir+'/less/*.less'],['less'])
-    gulp.watch([baseDir+'/*.html']).on('change', reload);
+  browserSync.init({
+    notify: false,
+    port: 9001,
+    open: false,
+    server: {
+      baseDir: [baseDir],
+    }
+  })
+  gulp.watch([baseDir + '/less/*.less'], ['less'])
+  gulp.watch([baseDir + '/*.html']).on('change', reload);
 });
 
 
-gulp.task('less',function(){
-	return gulp.src([baseDir+'/{,*}/less/*.less','!'+baseDir+'/{,*}/less/_*.less'],{
-            base:baseDir+'/less'
-        })
-        .pipe($.plumber({errorHandler: $.notify.onError('Error: <%= error.message %>')}))
-        .pipe($.less())
-        .pipe(gulp.dest(baseDir+'/styles/'))
-        .pipe(reload({stream: true}))
-})
+gulp.task('less', function() {
+  return gulp.src([baseDir + '/{,*}/less/*.less', '!' + baseDir + '/{,*}/less/_*.less'], {
+      base: baseDir + '/less'
+    })
+    .pipe($.plumber({ errorHandler: $.notify.onError('Error: <%= error.message %>') }))
+    .pipe($.less())
+    .pipe($.autoprefixer({
+      browsers: ['last 2 version']
+    }))
+    .pipe(gulp.dest(baseDir + '/styles/'))
+});
 
 
-gulp.task('build',function(){})
+gulp.task('build', function() {
+  return buildLess(baseDir)
+    .pipe(gulp.dest('dist/'))
+    .pipe(buildScript(baseDir))
+    .pipe($.logger())
+    .pipe(gulp.dest('dist/'))
+});
+
+gulp.task('buildAll', function() {
+
+});
+
+
+function buildLess(dir) {
+  return gulp.src([dir + '/{,*}/less/*.less', '!' + dir + '/{,*}/less/_*.less'], {
+      base: 'src'
+    })
+    .pipe($.plumber({ errorHandler: $.notify.onError('Error: <%= error.message %>') }))
+    .pipe($.less())
+    .pipe($.autoprefixer({
+      browsers: ['last 2 version']
+    }))
+}
+
+function buildScript(dir) {
+  return gulp.src([dir + '/scripts/{,*/}*.js', '!' + dir + '/scripts/libs/*.js'],{
+    base:'src'
+  })
+    .pipe($.uglify())
+}
+
+function copyAssets(){
+    
+}
