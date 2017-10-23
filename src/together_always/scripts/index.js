d61a07a73a72305c23094618da1661763fd265ef
abel <abel@abeldeMacBook-Pro.local>
2017-10-23 19:24:23 +0800

    update

--- src/together_always/scripts/index.js
+++ src/together_always/scripts/index.js
@@ -1,3 +1,4 @@
+
 var queue = new createjs.LoadQueue();
 queue.on("complete", handleComplete, this);
 queue.on("progress", function(e) {
@@ -21,10 +22,19 @@ queue.loadManifest([
   "./images/page1/tree.png",
   "./images/page1/play.png",
   "./images/page1/role.png",
+  "./images/bg.jpg",
   "./images/page1/river.jpg",
   "./images/page2/role.png",
   "./images/page2/left.png",
   "./images/page2/right.png",
+  "./images/page6/small_stone.png",
+  "./images/page4/bg.jpg",
+  "./images/page5/bg.jpg",
+  "./images/page6/bg.jpg",
+  "./images/page5/bg_tree.png",
+  "./images/page5/left_tree.png",
+  "./images/page5/right_tree.png",
+  "./images/page5/center_tree.png",
   {
     id: "sound",
     src: "./media/Together-Always.mp3",
@@ -32,58 +42,65 @@ queue.loadManifest([
 ]);
 
 function handleComplete() {
-	var sounds = [{
-      src: "./media/Together-Always.mp3",
-      data: {
-          audioSprite: [
-              { id: "sound1", startTime: 5000, duration: 18000 },
-              { id: "sound2", startTime: 24000, duration: 8220 },
-              { id: "sound2.2", startTime: 55250, duration: 10520 },
-              { id: "sound3", startTime: 66180, duration: 3990 },
-              { id: "sound4", startTime: 70014, duration: 1999 },
-              { id: "sound5", startTime: 72014, duration: 2006 },
-              { id: "sound6", startTime: 89021, duration: 5999 },//page3
-              { id: "sound7", startTime: 95021, duration: 1980 },
-              { id: "sound7.1", startTime: 98027, duration: 6987 },
-              {id:"sound8",startTime:105015,duration:2993},
-              {id:"sound9",startTime:110010,duration:2013},
-              {id:"sound10",startTime:114002,duration:1027},
-              {id:"sound11",startTime:115030,duration:2973},
-              {id:"sound12",startTime:118004,duration:21021},//page4
-              {id:"sound13",startTime:139026,duration:2489},//page5
-              {id:"sound14",startTime:142017,duration:2004},
-              {id:"sound32",startTime:145228,duration:2800},
-              {id:"sound15",startTime:149028,duration:2899},
-              {id:"sound15.1",startTime:153201,duration:2320},
-              {id:"sound16",startTime:157004,duration:2520},
-              {id:"sound17",startTime:159725,duration:3975},
-              {id:"sound18",startTime:163001,duration:41021},//page6
-              {id:"sound19",startTime:233824,duration:9979},//page7
-              {id:"sound20",startTime:258002,duration:9425},
-              {id:"sound21",startTime:267828,duration:3989},//page8
-              {id:"sound33",startTime:274000,duration:3820},
-              {id:"sound22",startTime:272819,duration:1983},
-              {id:"sound23",startTime:279003,duration:1321},
-              {id:"sound24",startTime:311014,duration:13610},//page9
-              {id:"sound25",startTime:324825,duration:3199},
-              {id:"sound26",startTime:329024,duration:3580},
-              {id:"sound27",startTime:333814,duration:2188},
-              {id:"sound27.1",startTime:337024,duration:1990},
-              {id:"sound28",startTime:338915,duration:2193},
-              {id:"sound28.1",startTime:342512,duration:2000},
-              {id:"sound29",startTime:344713,duration:17991},//page10
-              {id:"sound30",startTime:362705,duration:2609},
-              {id:"sound31",startTime:365015,duration:2996},
-          ]
-      }
+  var sounds = [{
+    src: "./media/Together-Always.mp3",
+    data: {
+      audioSprite: [
+        { id: "sound1", startTime: 5000, duration: 18000 },
+        { id: "sound2", startTime: 24000, duration: 8220 },
+        { id: "sound2.2", startTime: 55250, duration: 10520 },
+        { id: "sound3", startTime: 66180, duration: 2990 },
+        { id: "sound4", startTime: 70014, duration: 1999 },
+        { id: "sound5", startTime: 72014, duration: 2006 },
+        { id: "sound6", startTime: 89021, duration: 5999 }, //page3
+        { id: "sound7", startTime: 95021, duration: 1880 },
+        { id: "sound7.1", startTime: 98027, duration: 6987 },
+        { id: "sound8", startTime: 105015, duration: 2993 },
+        { id: "sound9", startTime: 110010, duration: 2713 },
+        { id: "sound10", startTime: 114002, duration: 1027 },
+        { id: "sound11", startTime: 115030, duration: 2973 },
+        { id: "sound12", startTime: 118004, duration: 21021 }, //page4
+        { id: "sound13", startTime: 139026, duration: 2489 }, //page5
+        { id: "sound14", startTime: 142017, duration: 2004 },
+        { id: "sound32", startTime: 145228, duration: 2800 },
+        { id: "sound15", startTime: 149028, duration: 2899 },
+        { id: "sound15.1", startTime: 153201, duration: 2320 },
+        { id: "sound16", startTime: 157004, duration: 2520 },
+        { id: "sound17", startTime: 159725, duration: 3975 },
+        { id: "sound18", startTime: 163001, duration: 41021 }, //page6
+        { id: "sound19", startTime: 233824, duration: 9979 }, //page7
+        { id: "sound20", startTime: 258002, duration: 9425 },
+        { id: "sound21", startTime: 267828, duration: 3989 }, //page8
+        { id: "sound33", startTime: 274000, duration: 3820 },
+        { id: "sound22", startTime: 272819, duration: 1983 },
+        { id: "sound23", startTime: 279003, duration: 1321 },
+        { id: "sound24", startTime: 311014, duration: 13610 }, //page9
+        { id: "sound25", startTime: 324825, duration: 3199 },
+        { id: "sound26", startTime: 329024, duration: 3580 },
+        { id: "sound27", startTime: 333814, duration: 2188 },
+        { id: "sound27.1", startTime: 337024, duration: 1990 },
+        { id: "sound28", startTime: 338915, duration: 2193 },
+        { id: "sound28.1", startTime: 342512, duration: 2000 },
+        { id: "sound29", startTime: 344713, duration: 17991 }, //page10
+        { id: "sound30", startTime: 362705, duration: 2609 },
+        { id: "sound31", startTime: 365015, duration: 2996 },
+      ]
+    }
   }];
   createjs.Sound.registerSounds(sounds);
-  
-  $('.loading-page').hide()
+
+  $('.loading-page').hide();
+
+  $('[original-src]').each(function() {
+    $(this).attr('src', $(this).attr('original-src'))
+  });
+
+  $('.pages').show();
+
+  FastClick.attach(document.body);
 };
 
 $(function() {
-
   $('.pages').width(window.innerWidth * $('.pages .page').length)
 
   $('.pages .page').width(window.innerWidth)
@@ -94,11 +111,6 @@ $(function() {
   })
 
   $(window).on('deviceorientation', function(e) {
-    console.log('absolute: ' + e.absolute)
-    console.log('alpha: ' + e.alpha)
-    console.log('beta: ' + e.beta)
-    console.log('gamma: ' + e.gamma)
-
 
     if (e.gamma != null && e.beta != null) {
 
@@ -159,9 +171,13 @@ $(function() {
       this.current = current;
     },
     next: function(next) {
+      if (this.current == this.$pages.length - 1) {
+        return false;
+      }
       this.move(-1)
     },
     pre: function(pre) {
+
       this.move(1)
     }
   }
@@ -179,56 +195,55 @@ $(function() {
     pages.pre()
   });
 
-  $('.play-button').on('click',function(){
-  	var number = $(this).attr('data-sound');
-  	play(number)
-  	console.log('play sound'+number)
+  $('.play-button').on('click', function() {
+    var number = $(this).attr('data-sound');
+    play(number)
+    console.log('play sound' + number)
   });
 
-  $('.talk-box').on('click','.talk-btn',function(){
+  $('.talk-box').on('click', '.talk-btn', function() {
 
-  	var $parent = $(this).parent();
-  	var index = $parent.data('index') || 0;
+    var $parent = $(this).parent();
+    var index = $parent.data('index') || 0;
 
-  	$parent.find('[data-talk="'+index+'"]').hide();
-  	index ++;
+    $parent.find('[data-talk="' + index + '"]').hide();
+    index++;
 
-  	if(index > $parent.attr('data-talk-cout')){
-  		return
-  	}
+    if (index > $parent.attr('data-talk-cout')) {
+      return
+    }
 
-  	if(index != $parent.attr('data-talk-cout')){
-  		if($(this).hasClass('talk-dialog-talk1') || $(this).hasClass('talk-dialog-hold1')){
-  			$parent.find('.talk-dialog-hold2').show()
-  		}else{
-  			$parent.find('.talk-dialog-hold1').show()
-  		}
-  	}
-  	var $next = $parent.find('[data-talk="'+index+'"]');
-  	$next.show();
+    if (index != $parent.attr('data-talk-cout')) {
+      if ($(this).hasClass('talk-dialog-talk1') || $(this).hasClass('talk-dialog-hold1')) {
+        $parent.find('.talk-dialog-hold2').show()
+      } else {
+        $parent.find('.talk-dialog-hold1').show()
+      }
+    }
+    var $next = $parent.find('[data-talk="' + index + '"]');
+    $next.show();
 
-  	if($next.attr('data-sound')){
-  		play($next.attr('data-sound'))
-  	}
+    if ($next.attr('data-sound')) {
+      play($next.attr('data-sound'))
+    }
 
-  	$(this).hide();
+    $(this).hide();
 
-  	$parent.data('index',index);
+    $parent.data('index', index);
   });
 
-  function play(number){
-  	console.log('play',number)
-  	createjs.Sound.stop();
-  	createjs.Sound.play("sound"+number).addEventListener('complete',function(){
-  		if( number ==  2 ){
-  			createjs.Sound.play("sound2.2")
-  		}
-  		else if( number ==  7 ){
-  			createjs.Sound.play("sound7.1")
-  		}else if( number ==  15 ){
-  			createjs.Sound.play("sound15.1")
-  		}
-  	});
+  function play(number) {
+    console.log('play', number)
+    createjs.Sound.stop();
+    createjs.Sound.play("sound" + number).addEventListener('complete', function() {
+      if (number == 2) {
+        createjs.Sound.play("sound2.2")
+      } else if (number == 7) {
+        createjs.Sound.play("sound7.1")
+      } else if (number == 15) {
+        createjs.Sound.play("sound15.1")
+      }
+    });
   }
 
 })
