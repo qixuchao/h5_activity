00da1cdeaf4302f5e4d8bb42bc2ac3e518aa7027
chenbo <chenbo@fancydigital.com.cn>
2018-01-12 10:33:20 +0800

    Feat: 百威问卷调查

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var $ = gulpLoadPlugins();
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
-var currentPath = 'shuke_activity/'
+var currentPath = 'budweiser/'
 
 /**
  * argv
