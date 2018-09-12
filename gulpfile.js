27aa6fe125c5d444e4b013ed990460f59eb12812
chenbo <chenbo@fancydigital.com.cn>
2018-09-12 11:08:35 +0800

    Feat: 中秋月饼活动

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var $ = gulpLoadPlugins();
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
-var currentPath = 'juneyao_new_year/' //
+var currentPath = 'jd_moon/' //
 
 /**
  * argv
