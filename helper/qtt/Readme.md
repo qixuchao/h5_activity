705d3dcb5c391cec9878636a86ba46d191142f31
abel <lijiang@fancydigital.com.cn>
2018-12-12 00:32:52 +0800

    checkout qttBridge

--- helper/qtt/Readme.md
+++ helper/qtt/Readme.md
@@ -21,3 +21,9 @@
 ```
 - dsbridge.js     dsbridge 基本类库
 - qttBridge.js    qttBridge 都方式封装
+
+#### jb.wx.qtt
+
+##### openShare 分享
+##### getToken 获得token 
+微信中是ssoToken，趣头条为qttTOken，return 统一为token
--- helper/qtt/qttBridge.js
+++ helper/qtt/qttBridge.js
@@ -1,18 +1,10 @@
-QTTB = (function(){
-	var bridge={default:this,call:function(b,a,c){var e="";"function"==typeof a&&(c=a,a={});a={data:void 0===a?null:a};if("function"==typeof c){var g="dscb"+window.dscb++;window[g]=c;a._dscbstub=g}a=JSON.stringify(a);if(window._dsbridge)e=_dsbridge.call(b,a);else if(window._dswk||-1!=navigator.userAgent.indexOf("_dsbridge"))e=prompt("_dsbridge="+b,a);return JSON.parse(e||"{}").data},register:function(b,a,c){c=c?window._dsaf:window._dsf;window._dsInit||(window._dsInit=!0,setTimeout(function(){bridge.call("_dsb.dsinit")},
+var bridge={default:this,call:function(b,a,c){var e="";"function"==typeof a&&(c=a,a={});a={data:void 0===a?null:a};if("function"==typeof c){var g="dscb"+window.dscb++;window[g]=c;a._dscbstub=g}a=JSON.stringify(a);if(window._dsbridge)e=_dsbridge.call(b,a);else if(window._dswk||-1!=navigator.userAgent.indexOf("_dsbridge"))e=prompt("_dsbridge="+b,a);return JSON.parse(e||"{}").data},register:function(b,a,c){c=c?window._dsaf:window._dsf;window._dsInit||(window._dsInit=!0,setTimeout(function(){bridge.call("_dsb.dsinit")},
 			0));"object"==typeof a?c._obs[b]=a:c[b]=a},registerAsyn:function(b,a){this.register(b,a,!0)},hasNativeMethod:function(b,a){return this.call("_dsb.hasNativeMethod",{name:b,type:a||"all"})},disableJavascriptDialogBlock:function(b){this.call("_dsb.disableJavascriptDialogBlock",{disable:!1!==b})}};
 			!function(){if(!window._dsf){var b={_dsf:{_obs:{}},_dsaf:{_obs:{}},dscb:0,dsBridge:bridge,close:function(){bridge.call("_dsb.closePage")},_handleMessageFromNative:function(a){var e=JSON.parse(a.data),b={id:a.callbackId,complete:!0},c=this._dsf[a.method],d=this._dsaf[a.method],h=function(a,c){b.data=a.apply(c,e);bridge.call("_dsb.returnValue",b)},k=function(a,c){e.push(function(a,c){b.data=a;b.complete=!1!==c;bridge.call("_dsb.returnValue",b)});a.apply(c,e)};if(c)h(c,this._dsf);else if(d)k(d,this._dsaf);
 			else if(c=a.method.split("."),!(2>c.length)){a=c.pop();var c=c.join("."),d=this._dsf._obs,d=d[c]||{},f=d[a];f&&"function"==typeof f?h(f,d):(d=this._dsaf._obs,d=d[c]||{},(f=d[a])&&"function"==typeof f&&k(f,d))}}},a;for(a in b)window[a]=b[a];bridge.register("_hasJavascriptMethod",function(a,b){b=a.split(".");if(2>b.length)return!(!_dsf[b]&&!_dsaf[b]);a=b.pop();b=b.join(".");return(b=_dsf._obs[b]||_dsaf._obs[b])&&!!b[a]})}}();
 
+QTTB = (function(){
 	var qkBridge = {
-	  getCommonMsg:function(){
-	    if (bridge.hasNativeMethod("getCommonMsg")) {
-	      var res = bridge.call("getCommonMsg"); //同步
-	      return (res && res.code === 1 && res.data) || ''
-	    } else {
-	      return (window.qukanClient && window.qukanClient.getCommonMsg && window.qukanClient.getCommonMsg()) || ''
-	    }
-	  },
 	  isCoinVersion:function(){
 	    if (bridge.hasNativeMethod("isCoinVersion")) {
 	      var res = bridge.call("isCoinVersion"); //同步
@@ -28,9 +20,18 @@ QTTB = (function(){
 	}
 
 	function isCoinVersion(){
-		return qkBridge.isCoinVersion()|| window.cpcAndroid && cpcAndroid.fetchInfo('isCoinVersion')
+		if(bridge.hasNativeMethod("isCoinVersion")){
+			var res = bridge.call("isCoinVersion"); //同步
+			return (res && res.code === 1 && res.data.value) || ''
+		}else if(window.qukanClient){
+			return !!window.qukanClient.isCoinVersion && window.qukanClient.isCoinVersion() 
+		}else{
+			return window.cpcAndroid && cpcAndroid.fetchInfo('isCoinVersion')
+		}
+		
 	}
 
+	// 老版走url 拦截
 	function getTokenBySchema(){
 		var calbackName = 'callbacks_'+Math.random().toString(16).slice(3,9)
 		window[calbackName] = function(token){
@@ -44,18 +45,25 @@ QTTB = (function(){
 		callback = callback || function(){{}}
 		if(window.cpcAndroid){
 			callback(cpcAndroid.fetchInfo('getToken'))
+		}else if(bridge.hasNativeMethod("getCommonMsg")){
+			var res = bridge.call("getCommonMsg"); //同步
+			if(res && res.code === 1 && res.data){
+				callback(res.data.token)
+			}else{
+				callback(null)
+			}
 		}else if(window.qukanClient){
-			callback(qkBridge.getCommonMsg().token)
+			callback(window.qukanClient && window.qukanClient.getCommonMsg && window.qukanClient.getCommonMsg()) || ''
 		}else{
 			getTokenBySchema(callback)
 		}
 	}
 
 	var qb = {
-		version:'1.0.0',
+		version:'1.0.1',
 		isCoinVersion:isCoinVersion,
 		getToken:geToken,
-		isQTT: /qukan/.test(navigator.userAgent)
+		isQTT: /qukan|clicash_android/.test(navigator.userAgent)
 	}
 
 	return qb
